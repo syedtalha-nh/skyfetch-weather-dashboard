973baa65adf79e9308067e294de1799737591a92
syedtalha-nh <syedtalha.nh@gmail.com>
2026-02-26 09:05:51 +0530

    Add weather fetching functionality in app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const apiKey = "YOUR_API_KEY";
+const city = "London";
+
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl)
+  .then(function(response) {
+    
+    const data = response.data;
+
+    const cityName = data.name;
+    const temperature = data.main.temp;
+    const description = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+
+    document.getElementById("city-name").innerText = cityName;
+    document.getElementById("temperature").innerText = `Temperature: ${temperature}°C`;
+    document.getElementById("description").innerText = `Condition: ${description}`;
+    document.getElementById("weather-icon").src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+  })
+  .catch(function(error) {
+    console.error("Error fetching weather data:", error);
+  });
